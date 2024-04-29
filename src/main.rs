8970a762af635250e2519af183b01ba64c4c2b44
Shannon Skipper <shannonskipper@gmail.com>
2024-04-29 15:33:25 -0700

    Support input from stdin

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,20 @@
 
 #[unix_sigpipe = "sig_dfl"]
 fn main() {
-    let cli = Cli::parse();
-    let word_tally = WordTally::new(&cli.path, !cli.no_sort);
+    let args = Args::parse();
+    let word_tally = WordTally::new(&args.input, !args.no_sort);
 
-    if cli.verbose {
-        println!("path: {}", word_tally.path);
+    if args.verbose {
+        println!("source: {:#?}", args.input.source);
         println!("sorted: {}", word_tally.sorted);
     }
 
-    if cli.debug {
-        println!("verbose: {}", cli.verbose);
-        println!("debug: {}", cli.debug);
+    if args.debug {
+        println!("verbose: {}", args.verbose);
+        println!("debug: {}", args.debug);
     }
 
-    if cli.verbose || cli.debug {
+    if args.verbose || args.debug {
         println!();
     }
 
@@ -33,12 +33,14 @@ fn main() {
     }
 }
 
-use clap::Parser;
+use clap::{Parser, ValueHint};
+use clap_stdin::FileOrStdin;
 
 #[derive(Debug, Parser)]
-#[command(version, about)]
-struct Cli {
-    path: String,
+#[command(about, version)]
+struct Args {
+    #[clap(default_value = "-", value_hint = ValueHint::FilePath)]
+    input: FileOrStdin,
     #[arg(short, long, help = "Print additional details")]
     verbose: bool,
     #[arg(short, long, help = "Print unsorted word count")]
@@ -49,26 +51,22 @@ struct Cli {
 
 use core::cmp::Reverse;
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path;
+use std::io::{self, BufRead, BufReader, Lines, Read};
 use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Clone, Debug)]
 #[non_exhaustive]
 pub struct WordTally {
-    pub path: String,
     pub sorted: bool,
     pub tally: Vec<(String, u64)>,
 }
 
 impl WordTally {
     #[must_use]
-    pub fn new(path: &str, sort: bool) -> Self {
+    pub fn new(input: &FileOrStdin, sort: bool) -> Self {
         let mut word_tally = Self {
-            path: Self::absolut_path(path),
             sorted: false,
-            tally: Vec::from_iter(Self::tally(Self::lines(path))),
+            tally: Vec::from_iter(Self::tally(Self::lines(input))),
         };
 
         if sort {
@@ -88,22 +86,18 @@ impl WordTally {
         self.sorted = true;
     }
 
-    fn absolut_path(path: &str) -> String {
-        path::absolute(path)
-            .expect("Absolute path not available")
-            .into_os_string()
-            .into_string()
-            .expect("Absolute path encoding invalid")
+    fn lines(input: &FileOrStdin) -> Lines<BufReader<impl Read>> {
+        let reader = input.into_reader();
+        assert!(
+            reader.is_ok(),
+            "Input source unreadable: {:#?}",
+            input.source
+        );
+
+        io::BufReader::new(reader.unwrap()).lines()
     }
 
-    fn lines(path: &str) -> io::Lines<io::BufReader<File>> {
-        let file = File::open(path);
-        assert!(file.is_ok(), "File not readable: {path}");
-
-        io::BufReader::new(file.unwrap()).lines()
-    }
-
-    fn tally(lines: io::Lines<io::BufReader<File>>) -> HashMap<String, u64> {
+    fn tally(lines: io::Lines<BufReader<impl Read>>) -> HashMap<String, u64> {
         let mut tally = HashMap::new();
 
         for line in lines.map_while(Result::ok) {
