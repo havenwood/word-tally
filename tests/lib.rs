7c54c6717f3c887867bbb43c56262f71ab9296d2
Shannon Skipper <shannonskipper@gmail.com>
2024-05-16 11:41:25 -0700

    Refactor `WordTally` field tests to be more DRY

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,108 +3,117 @@ use std::hash::{DefaultHasher, Hash, Hasher};
 use std::str::FromStr;
 use word_tally::*;
 
+const WORDS_PATH: &str = "tests/files/words.txt";
+
+struct Fields<'a> {
+    count: u64,
+    uniq_count: usize,
+    avg: f64,
+    tally: Vec<(&'a str, u64)>,
+}
+
+fn word_tally_test(case: Case, sort: Sort, fields: Fields) {
+    if let Ok(file_or_stdin) = FileOrStdin::from_str(WORDS_PATH) {
+        let word_tally = WordTally::new(&file_or_stdin, case, sort).unwrap();
+
+        assert_eq!(word_tally.count(), fields.count);
+        assert_eq!(word_tally.uniq_count(), fields.uniq_count);
+        assert_eq!(word_tally.avg().unwrap(), fields.avg);
+
+        let expected_tally: Vec<(String, u64)> = fields
+            .tally
+            .iter()
+            .map(|(word, count)| ((*word).to_string(), *count))
+            .collect();
+        assert_eq!(word_tally.tally(), &expected_tally);
+    }
+}
+
 #[test]
 fn case_insensitive_desc_order() {
-    if let Ok(file_or_stdin) = FileOrStdin::from_str("tests/files/words.txt") {
-        let word_tally = WordTally::new(&file_or_stdin, Case::Lower, Sort::Desc).unwrap();
+    word_tally_test(
+        Case::Lower,
+        Sort::Desc,
+        Fields {
+            count: 45,
+            uniq_count: 5,
+            avg: 9.0,
+            tally: vec![("c", 15), ("d", 11), ("123", 9), ("b", 7), ("a", 3)],
+        },
+    );
 
-        assert_eq!(word_tally.count(), 45);
-        assert_eq!(word_tally.uniq_count(), 5);
-        assert_eq!(word_tally.avg().unwrap(), 9.0);
-        assert_eq!(
-            word_tally.tally(),
-            &vec![
-                ("c".to_string(), 15),
-                ("d".to_string(), 11),
-                ("123".to_string(), 9),
-                ("b".to_string(), 7),
-                ("a".to_string(), 3)
-            ]
-        );
-
-        let upper = WordTally::new(&file_or_stdin, Case::Upper, Sort::Desc).unwrap();
-        assert_eq!(
-            upper.tally(),
-            &vec![
-                ("C".to_string(), 15),
-                ("D".to_string(), 11),
-                ("123".to_string(), 9),
-                ("B".to_string(), 7),
-                ("A".to_string(), 3)
-            ]
-        )
-    }
+    word_tally_test(
+        Case::Upper,
+        Sort::Desc,
+        Fields {
+            count: 45,
+            uniq_count: 5,
+            avg: 9.0,
+            tally: vec![("C", 15), ("D", 11), ("123", 9), ("B", 7), ("A", 3)],
+        },
+    );
 }
 
 #[test]
 fn case_insensitive_asc_order() {
-    if let Ok(file_or_stdin) = FileOrStdin::from_str("tests/files/words.txt") {
-        let word_tally = WordTally::new(&file_or_stdin, Case::Lower, Sort::Asc).unwrap();
-
-        assert_eq!(word_tally.count(), 45);
-        assert_eq!(word_tally.uniq_count(), 5);
-        assert_eq!(word_tally.avg().unwrap(), 9.0);
-        assert_eq!(
-            word_tally.tally(),
-            &vec![
-                ("a".to_string(), 3),
-                ("b".to_string(), 7),
-                ("123".to_string(), 9),
-                ("d".to_string(), 11),
-                ("c".to_string(), 15),
-            ]
-        );
-    }
+    word_tally_test(
+        Case::Lower,
+        Sort::Asc,
+        Fields {
+            count: 45,
+            uniq_count: 5,
+            avg: 9.0,
+            tally: vec![("a", 3), ("b", 7), ("123", 9), ("d", 11), ("c", 15)],
+        },
+    );
 }
 
 #[test]
 fn case_sensitive_desc_order() {
-    if let Ok(file_or_stdin) = FileOrStdin::from_str("tests/files/words.txt") {
-        let word_tally = WordTally::new(&file_or_stdin, Case::Original, Sort::Desc).unwrap();
-
-        assert_eq!(word_tally.count(), 45);
-        assert_eq!(word_tally.uniq_count(), 9);
-        assert_eq!(word_tally.avg().unwrap(), 5.0);
-        assert_eq!(
-            word_tally.tally(),
-            &vec![
-                ("123".to_string(), 9),
-                ("C".to_string(), 8),
-                ("c".to_string(), 7),
-                ("D".to_string(), 6),
-                ("d".to_string(), 5),
-                ("B".to_string(), 4),
-                ("b".to_string(), 3),
-                ("A".to_string(), 2),
-                ("a".to_string(), 1)
-            ]
-        );
-    }
+    word_tally_test(
+        Case::Original,
+        Sort::Desc,
+        Fields {
+            count: 45,
+            uniq_count: 9,
+            avg: 5.0,
+            tally: vec![
+                ("123", 9),
+                ("C", 8),
+                ("c", 7),
+                ("D", 6),
+                ("d", 5),
+                ("B", 4),
+                ("b", 3),
+                ("A", 2),
+                ("a", 1),
+            ],
+        },
+    );
 }
 
 #[test]
 fn case_sensitive_asc_order() {
-    if let Ok(file_or_stdin) = FileOrStdin::from_str("tests/files/words.txt") {
-        let word_tally = WordTally::new(&file_or_stdin, Case::Original, Sort::Asc).unwrap();
-
-        assert_eq!(word_tally.count(), 45);
-        assert_eq!(word_tally.uniq_count(), 9);
-        assert_eq!(word_tally.avg().unwrap(), 5.0);
-        assert_eq!(
-            word_tally.tally(),
-            &vec![
-                ("a".to_string(), 1),
-                ("A".to_string(), 2),
-                ("b".to_string(), 3),
-                ("B".to_string(), 4),
-                ("d".to_string(), 5),
-                ("D".to_string(), 6),
-                ("c".to_string(), 7),
-                ("C".to_string(), 8),
-                ("123".to_string(), 9),
-            ]
-        );
-    }
+    word_tally_test(
+        Case::Original,
+        Sort::Asc,
+        Fields {
+            count: 45,
+            uniq_count: 9,
+            avg: 5.0,
+            tally: vec![
+                ("a", 1),
+                ("A", 2),
+                ("b", 3),
+                ("B", 4),
+                ("d", 5),
+                ("D", 6),
+                ("c", 7),
+                ("C", 8),
+                ("123", 9),
+            ],
+        },
+    )
 }
 
 #[test]
@@ -148,7 +157,7 @@ fn equality_and_hashing() {
 
 #[test]
 fn vec_from() {
-    if let Ok(file_or_stdin) = FileOrStdin::from_str("tests/files/words.txt") {
+    if let Ok(file_or_stdin) = FileOrStdin::from_str(WORDS_PATH) {
         let tally = WordTally::new(&file_or_stdin, Case::Lower, Sort::Desc).unwrap();
 
         assert_eq!(
